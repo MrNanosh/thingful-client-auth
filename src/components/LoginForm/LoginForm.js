2b697946584122c05654198ec895de4c999038e8
Dana Zinatbakhsh <dtzinatbakhsh@gmail.com>
2019-12-20 05:47:04 -0800

    basic auth and hashing done with most tests passing

--- src/components/LoginForm/LoginForm.js
+++ src/components/LoginForm/LoginForm.js
@@ -1,60 +1,82 @@
-import React, { Component } from 'react'
-import { Button, Input } from '../Utils/Utils'
+import React, {
+  Component
+} from 'react';
+import {
+  Button,
+  Input
+} from '../Utils/Utils';
+import TokenService from '../../services/token-service';
 
 export default class LoginForm extends Component {
   static defaultProps = {
     onLoginSuccess: () => {}
-  }
+  };
 
-  state = { error: null }
+  state = { error: null };
 
   handleSubmitBasicAuth = ev => {
-    ev.preventDefault()
-    const { user_name, password } = ev.target
+    ev.preventDefault();
+    const {
+      user_name,
+      password
+    } = ev.target;
 
-    console.log('login form submitted')
-    console.log({ user_name, password })
+    TokenService.saveAuthToken(
+      TokenService.makeBasicAuthToken(
+        user_name.value,
+        password.value
+      )
+    );
+    console.log(
+      'username and password taken'
+    );
 
-    user_name.value = ''
-    password.value = ''
-    this.props.onLoginSuccess()
-  }
+    user_name.value = '';
+    password.value = '';
+    this.props.onLoginSuccess();
+  };
 
   render() {
-    const { error } = this.state
+    const { error } = this.state;
     return (
       <form
-        className='LoginForm'
-        onSubmit={this.handleSubmitBasicAuth}
+        className="LoginForm"
+        onSubmit={
+          this.handleSubmitBasicAuth
+        }
       >
-        <div role='alert'>
-          {error && <p className='red'>{error}</p>}
+        <div role="alert">
+          {error && (
+            <p className="red">
+              {error}
+            </p>
+          )}
         </div>
-        <div className='user_name'>
-          <label htmlFor='LoginForm__user_name'>
+        <div className="user_name">
+          <label htmlFor="LoginForm__user_name">
             User name
           </label>
           <Input
             required
-            name='user_name'
-            id='LoginForm__user_name'>
-          </Input>
+            name="user_name"
+            id="LoginForm__user_name"
+          ></Input>
         </div>
-        <div className='password'>
-          <label htmlFor='LoginForm__password'>
+        <div className="password">
+          <label htmlFor="LoginForm__password">
             Password
           </label>
           <Input
             required
-            name='password'
-            type='password'
-            id='LoginForm__password'>
-          </Input>
+            name="password"
+            type="password"
+            id="LoginForm__password"
+          ></Input>
         </div>
-        <Button type='submit'>
+        <Button type="submit">
           Login
         </Button>
       </form>
-    )
+    );
   }
 }
